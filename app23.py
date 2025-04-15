8f440b5c7be3b8abc04e4f66cf98cecefbb89dd3
Sief Mohamed Shama <siefshama@gmail.com>
2025-04-15 18:49:22 +0200

    Add files via upload

--- app23.py
+++ app23.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
+import plotly.figure_factory as ff
 from scipy.stats import gaussian_kde
 import os
 import requests
